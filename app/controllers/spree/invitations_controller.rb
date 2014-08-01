a21db869e19aace0e8d419832511227a68267de5
Alessio Rocco <alessio.rocco.lt@gmail.com>
2014-08-01 11:07:15 +0200

    adds layout to devise invitable views

--- app/controllers/spree/invitations_controller.rb
+++ app/controllers/spree/invitations_controller.rb
@@ -1,2 +1,7 @@
-class Spree::InvitationsController < Devise::InvitationsController
+module Spree
+  class InvitationsController < Devise::InvitationsController
+    include Spree::Core::ControllerHelpers::Common
+    include Spree::Core::ControllerHelpers::Store
+    helper 'spree/base', 'spree/store'
+  end
 end
