d0de1dfc7a151922a520b9ef06ea0b4505e17866
Alessio Rocco <alessio.rocco.lt@gmail.com>
2015-01-30 11:12:25 +0100

    changes invitation route

--- app/overrides/invitation_link.rb
+++ app/overrides/invitation_link.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(virtual_path: 'spree/shared/_main_nav_bar',
                      name: 'invitation_link',
                      insert_after: '#home-link',
-                     text: "<li><%= link_to('Invite a friend', new_spree_user_invitation_path) %></li>")
+                     text: "<li><%= link_to('Invite a friend', custom_invitation_path) %></li>")
